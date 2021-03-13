0191452a0066af650105d9fa6985fdc9058f4325
Michael Fait <mfait@thoughtworks.com>
2021-03-13 16:37:47 +0100

    add matches endpoint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
+function MatchItem(props) {
+  const match=props.match;
+  return (
+    <li>{match.gameId} | {match.gameType}</li>
+  );
+}
+
+function MatchList(props) {
+  const matches = props.matches;
+  const listItems = matches.map((match) =>
+    <MatchItem key={match.gameId} match={match} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [currentMatches, setCurrentMatches] = useState(1);
 
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
+    })
+  }, []);
+
+  useEffect(() => {
+    fetch('/matches').then(res => res.json()).then(data => {
+      setCurrentMatches(data.results);
     })
   }, []);
 
@@ -18,12 +42,7 @@ function App() {
     <div className="App">
       <header className="App-header"> 
         <p>The current time is {currentTime}.</p>
-        <ul>
-          <li className="Team">Team A</li>
-          <li>Team B</li>
-          <li>Team C</li>
-          <li>Team D</li>
-        </ul>
+        <MatchList matches={currentMatches} />
       </header>
     </div>
   );
