fb2361aacedace6a377f30b82d9aed02fc3f068c
Michael Fait <mfait@thoughtworks.com>
2021-03-13 16:11:27 +0100

    add matches endpoint

--- api/api.py
+++ api/api.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -7,4 +7,11 @@ app = Flask(__name__)
 def get_current_time():
     return {'time': time.time()}
 
-
+@app.route('/matches')
+def get_current_matches():
+    matches = [
+        {'gameId': 2, 'gameType': 'MATCHED_GAME'},
+        {'gameId': 3, 'gameType': 'MATCHED_GAME'},
+        {'gameId': 4, 'gameType': 'CUSTOM_GAME'}        
+    ]
+    return jsonify(results = matches)
